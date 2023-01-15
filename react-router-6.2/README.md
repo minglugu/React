b445714c0d6effcba727234c185bc5b5f4610f04
Minglu Gu <gmlsyjsh@gmail.com>
2023-01-15 13:53:19 -0400

    react: use routers to link webpages

--- react-router-6.2/README.md
+++ react-router-6.2/README.md
@@ -9,4 +9,23 @@ npm start
         <Route path='/page3/:id' element={<Page3/>}/>
 
 
-@1:11:30
+git add .
+git commit -m"added router features"
+git push 后会出现类似这样的提示：git push --set-upstream origin nameInitials-router-2022-02-28
+复制提示以后，在push，就push到了GitLab上了。
+
+开始做master 或 main的新的feature：
+git checkout master
+git pull 
+git fetch                       // 更新local的分支库
+git branch --all                //找到自己的分支名
+输入q，就quit，进入项目文件夹下，进入自己的分支下work
+git checkout lu-router-2023-01-01（自己的分支名）
+
+布置一下作业：
+
+
+
+
+
+
--- react-router-6.2/src/App.js
+++ react-router-6.2/src/App.js
@@ -35,7 +35,11 @@ const ProductPage = () => {
       {/* 前面3个button用的是useNavigate()这个hook，用来页面之间的跳转
           -1,表示往前面一个跳转 */}
       <button onClick={() => navigate(-1)}>go back</button>
-      <button onClick={() => navigate('/page4')}>Go to page 4</button>
+      {/* 做成按钮，跳转到相关页面. 并且可以传state 这个参数。
+          对nagivate()这个function的解释：we call the navigate function when clicking this button
+          with the name of the route (here is '/page3') that moves the user to. 
+          然后在const page3 里面, 将state 传给 location*/}
+      <button onClick={() => navigate('/page3', {state: {id: 7, color: 'green'}})}>Go to page 3</button>
       <button onClick={() => navigate('/')}>Go to Main Page</button>
       {/* Windows自带的function，可以useNavigate也可以用Windows自带的网页跳转的方法。 */}
       <button onClick={() => window.history.go(-1)}>Go -1</button>
@@ -48,8 +52,25 @@ const ProductPage = () => {
 const Page3= () => {
   const products = ['product1', 'product2', 'product3']
   // const navigate = useNavigate()
+  // useLocation hook
   const location = useLocation()
-  const search = location.search
+  // location is an object, with key-value pairs as following:
+  /*
+    hash: ""
+    key: "3tkj4ef7"
+    pathname: "/page3"      useLocation().pathname to find the path of this webpage
+    search: ""
+    state: null             how to pass the state(e.g. array or object)
+  */
+ // 如果点击NavBar 的page3跳转，那么会报错。因为没有传parameter，所以加判断以后，就不会报错了。
+ // 如何验证？点击NavBar的Main Page，然后点击 Page 3，会发现 state: null
+  console.log('location of page3 is ', location)
+  if (!!location.state) {
+    const {state:{id, color}} = location
+    console.log('id is', id, '\ncolor is', color)
+  }
+
+  // const search = location.search
   // const item = new URLSearchParams(search).get('item')
 
   return(
@@ -57,7 +78,8 @@ const Page3= () => {
       <h2>Page3</h2>
       {products.map((product, index) => {
         return(
-          <div>
+          // Warning: Each child in a list should have a unique "key" prop. Here we add key={index} to <div>
+          <div key={index}>
             {/* 包裹在Link里，当点击以后，会跳转到相关页面. 有${}, 用``backtick */}
             <Link to={`/page3/product_details/${product}`} key={index}>View {product}</Link>
           </div>
@@ -83,11 +105,22 @@ const NameDetails = () => {
   return <h2>Details of name {productName.name}</h2>
 }
 
+// 用户输入了一个无效链接，1. 那么就定义这个webpage，类似 Not Found
 const NoMatch = () => {
   return (
     <div>
       <h2>Nothing to see here!</h2>
       <p><Link to='/'>Go to Main Page</Link></p>
+    </div>
+  )
+}
+
+// 用户输入了一个无效链接，2. 或者定义一个Page404的webpage
+const Page404 = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Oops! This Page Not Found!</p>
     </div>
   )
 }
@@ -108,6 +141,7 @@ const NavBar = () => {
       <Link to='/'>Main Page</Link>
       <Link to='/page1'>Page 1</Link>
       <Link to='/Product-Page/100'>Product Page</Link>
+      {/* 也可以通过Link，跳转到相关页面 */}
       <Link to='/page3'>Page 3</Link>
     </nav>
   )
@@ -124,8 +158,12 @@ function App() {
         {/* 不是仅仅跳转到page3，还要传个值。此处是传个id。
             点开歌曲链接，单独进入一个播放的页面。 */}
         <Route path='/Product-Page/:id' element={<ProductPage/>}/>
-        3<Route path='/page3/*' element={<Page3/>}/>
-        <Route path='*' element={<NoMatch/>}/>
+        {/* if a webpage has a nested Route, use "*" to present all the elements */}
+        <Route path='/page3/*' element={<Page3/>}/>
+        {/* NoMatch的路径是'*'  */}
+        {/* <Route path='*' element={<NoMatch/>}/> */}
+        {/* 还可以define一个 page 404 */}
+        <Route path='*' element={<Page404/>}/>
       </Routes>
     </div>
   )
